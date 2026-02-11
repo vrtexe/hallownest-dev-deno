4db9bbfd09239bc02d49edd7a59446cf408cd4ca
vrtexe <traykovski5@gmail.com>
2026-02-11 02:21:41 +0100

    Update deno and startup script

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import { serveDir } from "@std/http/file-server";
 
 if (import.meta.main) {
   Deno.serve({ port: 3000 }, (req) => {
-    return serveDir(req);
+    return serveDir(req, {
+      fsRoot: "./dist",
+    });
   });
 }
-
-
